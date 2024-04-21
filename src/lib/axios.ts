409a817de97e12db6ca642ebacca4d687c998c63
arya-bayu <aryabayu00@proton.me>
2024-04-22 02:40:56 +0800

    Fix Axios

--- src/lib/axios.ts
+++ src/lib/axios.ts
@@ -4,6 +4,7 @@ const baseURL = process.env.NEXT_PUBLIC_BACKEND_URL
 
 const axios = Axios.create({
   withCredentials: true,
+  withXSRFToken: true,
   headers: {
     'X-Requested-With': 'XMLHttpRequest',
   },
