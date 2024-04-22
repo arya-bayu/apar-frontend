b877e854eda0d6cfbf19e2de476baac9263c2bb4
arya-bayu <aryabayu00@proton.me>
2024-04-22 11:48:35 +0800

    Add Menu ke Home

--- src/components/Layouts/AppLayout.tsx
+++ src/components/Layouts/AppLayout.tsx
@@ -7,7 +7,6 @@ import {
   LayoutDashboard,
   Building2,
   Boxes,
-  Settings,
   Package,
   Ruler,
   Users2Icon,
@@ -120,9 +119,9 @@ const AppLayout = ({
               />
             )}
             <SidebarItem
-              href="/dashboard/settings"
-              icon={<Settings size={20} />}
-              text="Pengaturan"
+              href="../"
+              icon={<svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="lucide lucide-square-mouse-pointer"><path d="M21 11V5a2 2 0 0 0-2-2H5a2 2 0 0 0-2 2v14a2 2 0 0 0 2 2h6" /><path d="m12 12 4 10 1.7-4.3L22 16Z" /></svg>}
+              text="Home"
             />
           </Sidebar>
 
